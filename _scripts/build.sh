e66baa4b0b89ffd95e5bc6d64c31436cd66b6b1d
Nurmukhamed Artykaly <nurmukh@nbc.kz>
2017-03-03 15:06:19 +0600

    Update build.sh
    Changing algorithm.
    
    Now I list data.txt file, parse every line, then check git, what commit is done.

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
 # check git status
+for line in $(cat data.txt); do
+	token=$(echo ${line}|cut -d ":" -f 1)
+	dnsdomain=$(echo ${line}|cut -d ":" -f 2)
+	ttl=$(echo ${line}|cut -d ":" -f 3)
+	record=$(echo ${line}|cut -d ":" -f 4)
+	subdomain=$(echo ${line}|cut -d ":" -f 5)
+	gpgkey=$(echo ${line}|cut -d ":" -f 6)
+	filename="${subdomain}.txt"
 
-for file in $(git status --porcelain); do
-gpgkey=$(cat links.txt|grep ${file}| cut -d ":" -f 2)
+	commit=$(git log --all -- '${filename}' $TRAVIS_COMMIT_RANGE | grep "commit" | head -n 1 | awk '{print $2}' )
 
-if [ -n "${gpgkey}" ]; then
-	commit=$(git log --all -- '${file}' | grep "commit" | head -n 1| awk '{print $2}')
-	
-	if [ -n "${commit}"]; then
-		gpgsign=$(git log --show-signature ${commit} | grep ${gpgkey}| head -n 1)
+	if [ -n "${commit}" ]; then
+		gpgsign=$(git log --show-signature ${commit} | grep ${gpgkey} | head -n 1)
 
 		if [ -n "${gpgsign}" ]; then
 			echo "Update Yandex DNA API"
-			dnsname="$(echo ${file}| cut -d "." -f 1).homeserver.kz"
-			ipaddress=$(cat $file)
-			dnsdomain="homeserver.kz"
-			ttl="900"
-			token="$(cat data.txt|grep ${name}|cut -d ":" -f 1)"
-			record="$(cat data.txt|grep ${name}|cut -d ":" -f 5)"
-			echo "DNSRECORD is ${dnsrecord}"
-			echo "IPAddress is ${ipaddress}"
-			$HOME/tools/update.sh -a ${token} -b ${dnsdomain} -t ${ttl} -r ${record} -d ${dnsname} -i ${ipaddress}
+			ipaddress=$(cat ${filename})
+			$HOME/tools/update.sh -a ${token} -b ${dnsdomain} -t ${ttl} -r ${record} -d ${subdomain} -i ${ipaddress}
 		else
 			echo "Found that file ${file} signed by other gpg-key"
 		fi
 	else
 		echo "No commits were found\n"
 	fi
-else
-	echo "Found file ${file} that do not have corresponding gpg-key"
-fi
-
-
 done
