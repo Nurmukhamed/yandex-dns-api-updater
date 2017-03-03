43165c5422108ecdbde51639a6cca2dd7ee9854f
Main.Homeserver.kz <main@homeserver.kz>
2017-03-03 15:51:49 +0600

    test build.sh

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -10,17 +10,18 @@ for line in $(cat data.txt); do
 	gpgkey=$(echo ${line}|cut -d ":" -f 6)
 	filename="${subdomain}.txt"
 
-	commit=$(git log --all -- '${filename}' | grep "commit" | head -n 1 | awk '{print $2}' )
+	commit=$(git log --all -- '${filename}' $TRAVIS_COMMIT_RANGE | grep "commit" | head -n 1 | awk '{print $2}' )
 
 	if [ -z "${commit}" ]; then
 		gpgsign=$(git log --show-signature ${commit} | grep ${gpgkey} | head -n 1)
-
+		echo ${gpgsign}
+		echo
 		if [ -z "${gpgsign}" ]; then
 			echo "Update Yandex DNA API"
 			ipaddress=$(cat ${filename})
 			$HOME/tools/update.sh -a ${token} -b ${dnsdomain} -t ${ttl} -r ${record} -d ${subdomain} -i ${ipaddress}
 		else
-			echo "Found that file ${file} signed by other gpg-key"
+			echo "Found that file ${filename} signed by other gpg-key"
 		fi
 	else
 		echo "No commits were found\n"
